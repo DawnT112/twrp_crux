cf5d87ccb2b159cd23b88612b5dca821b9642ad1
diudiuyc520 <121587602@qq.com>
2025-04-26 17:29:24 +0800

    Update and rename omni_crux.mk to twrp_crux.mk

--- twrp_crux.mk
+++ twrp_crux.mk
@@ -16,14 +16,9 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/xiaomi/crux/device.mk)
 
 PRODUCT_DEVICE := crux
-PRODUCT_NAME := omni_crux
+PRODUCT_NAME := twrp_crux
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi9 Pro 5G
 PRODUCT_MANUFACTURER := xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="crux-user 11 RKQ1.200826.002 V13.0.1.0.RFXCNXM release-keys"
-
-BUILD_FINGERPRINT := Xiaomi/crux/crux:11/RKQ1.200826.002/V13.0.1.0.RFXCNXM:user/release-keys
